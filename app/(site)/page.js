396ba91fe34aafd724be3594e18644f69638002c
Oleksandr Bilyi <billy.ua@gmail.com>
2026-08-16 10:24:40 +0300

    Update page.js

--- app/(site)/page.js
+++ app/(site)/page.js
@@ -53,7 +53,7 @@ export default async function HomePage({ searchParams }) {
           highlightSlug={highlightSlug}
         />
       )}
-            <p>Кордони ділянок і розташування поховань на мапі можуть дещо відрізнятися від реальних координат.</p>
+            <p style="font-size: 13px;">Кордони ділянок і розташування поховань на мапі можуть дещо відрізнятися від реальних координат.</p>
     </main>
   );
 }
