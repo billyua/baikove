43b7cde9d50bbfa9b205696deb2f582ee635abc0
Oleksandr Bilyi <billy.ua@gmail.com>
2026-08-29 10:44:21 +0300

    Update page.js

--- app/(site)/grave/[slug]/page.js
+++ app/(site)/grave/[slug]/page.js
@@ -123,6 +123,7 @@ export default async function GravePage({ params }) {
         )}
 
         {grave.photo_url && (
+        <h2>Фото могили</h2>
         <img
           src={grave.photo_url}
           alt={`Фото могили ${fullName}`}
